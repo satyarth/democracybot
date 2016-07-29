d63bdd93b205cfaa016c67677a5ca5c0a0098e15
Satyarth Mishra Sharma <mail@satyarth.me>
2016-07-29 16:59:56 +0300

    Add new_session decorator

--- democracybot.py
+++ democracybot.py
@@ -13,6 +13,28 @@ Session = recordclass('Session', ['exiler', 'exilee_id', 'votes'])
 Settings = recordclass('Settings', ['quorum', 'timer'])
 
 
+
+def new_session(func):
+    def inner(bot, update, *args, **kwargs):
+        chat_id = update.message.chat_id
+        if chat_id in sessions:
+            bot.sendMessage(chat_id, "Error: One #beef at a time, ladies 🐄🐮🐃🍴")
+            return
+        else:
+            return func(bot, update, *args, **kwargs)
+    return inner
+
+
+def active_session(func):
+    def inner(bot, update, *args, **kwargs):
+        chat_id = update.message.chat_id
+        if chat_id not in sessions:
+            bot.sendMessage(chat_id, text="Not in a session. Start some shit with /kick first.")
+            return
+        return func(bot, update, *args, **kwargs)
+    return inner
+
+
 def update_votes(bot, chat_id):
     s = sessions[chat_id]
     print(s.votes)
@@ -28,22 +50,18 @@ def init_settings(bot, chat_id):
     bot.sendMessage(chat_id, text="Bot settings initialized.\n" +
                                   "Quorum: " + str(quorum) + "\n"+
                                   "Timer: 60s")
-
+@new_session
 def start(bot, update):
     chat_id = update.message.chat_id
-    if chat_id not in sessions:
-        init_settings(bot, update.message.chat_id)
-        bot.sendMessage(update.message.chat_id, 
-            text=dedent('''\
-                        Hi! Prepare to be fucked by the long dick of the law.
-                        How it works:
-                        * Nominate someone for kicking by replying to their comment with /kick
-                        * Voting begins
-                        * In order for voting to be valid, a minimum number of participants (quorum) must be reached.
-                        * Once the quorum is reached, majority wins.'''))
-    else:
-        bot.sendMessage(chat_id, "Error: One #beef at a time, ladies 🐄🐮🐃🍴")
-        return
+    init_settings(bot, update.message.chat_id)
+    bot.sendMessage(update.message.chat_id, 
+        text=dedent('''\
+                    Hi! Prepare to be fucked by the long dick of the law.
+                    How it works:
+                    * Nominate someone for kicking by replying to their comment with /kick
+                    * Voting begins
+                    * In order for voting to be valid, a minimum number of participants (quorum) must be reached.
+                    * Once the quorum is reached, majority wins.'''))
 
 #TODO
 def update_settings(bot, update):
@@ -55,7 +73,7 @@ def update_settings(bot, update):
         init_settings(bot, chat_id)
     pass
 
-
+@new_session
 def kick(bot, update, job_queue):
     chat_id = update.message.chat_id
     if not update.message.reply_to_message:
@@ -64,9 +82,6 @@ def kick(bot, update, job_queue):
     exilee_id = update.message.reply_to_message.from_user.id
     exilee_username = update.message.reply_to_message.from_user.username
     exiler = update.message.from_user['username']
-    if chat_id in sessions:
-        bot.sendMessage(chat_id, "Error: One #beef at a time, ladies 🐄🐮🐃🍴")
-        return
     if chat_id not in settings:
         init_settings(bot, chat_id)
 
@@ -79,16 +94,6 @@ def kick(bot, update, job_queue):
     job_queue.put(job)
     jobs[chat_id] = job
     sessions[chat_id] = s
-
-
-def active_session(func):
-    def inner(bot, update, *args):
-        chat_id = update.message.chat_id
-        if chat_id in sessions:
-            return func(bot, update, *args)
-        else:
-            bot.sendMessage(update.message.chat_id, text="Not in a session. Start some shit with /kick first.")
-    return inner
 
 
 @active_session
